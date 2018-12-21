817c522a3285c2a0174b53a71669b03986b9e511
jakub.starzynski <jakub.starzynski@norbsoft.com>
2018-12-21 14:43:33 +0100

    ELSN-1547

--- app/src/main/java/com/elstatgroup/elstat/ebest/sample/MainActivity.kt
+++ app/src/main/java/com/elstatgroup/elstat/ebest/sample/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.elstatgroup.elstat.ebest.sample
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
-import com.elstatgroup.elstat.sdk.api.NexoError
-import com.elstatgroup.elstat.sdk.api.NexoVerificationListener
+import android.util.Log
+import com.elstatgroup.elstat.sdk.api.*
+import com.elstatgroup.elstat.sdk.api.NexoVerificationResult.NexoVerificationStatus.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private val executor = Executors.newFixedThreadPool(10)
 
     private val mBluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
@@ -26,25 +30,49 @@ class MainActivity : AppCompatActivity() {
 
         mBluetoothAdapter?.bluetoothLeScanner?.startScan(object : ScanCallback() {
             override fun onScanResult(callbackType: Int, result: ScanResult?) {
-                result?.
+                executor.execute {
+                    result?.device?.let {
+                        val verificationResult = NexoSync.getInstance().verifyBeacon(
+                            applicationContext, it, result.rssi)
+                        handleVerificationResult(it, verificationResult)
+                    }
+                }
+            }
+
+            override fun onScanFailed(errorCode: Int) {
+                super.onScanFailed(errorCode)
             }
         })
     }
 
-    private val verificationListener = object : NexoVerificationListener {
-
-        override fun onAuthorized(deviceName: String?, nexoId: String?) {
-            Toast.makeText(applicationContext, "Authorized: $deviceName", Toast.LENGTH_SHORT).show()
+    private fun handleVerificationResult(device: BluetoothDevice, result: NexoVerificationResult) {
+        when(result.status) {
+            NOT_AUTHORIZED -> Log.v("eBestSample", "unauthorized: ${device.name ?: device.address}")
+            AUTHORIZED -> result.nexoId?.let { nexoId ->
+                Log.v("eBestSample", "authorized: $nexoId")
+                NexoSync.getInstance().syncCooler(applicationContext, nexoId, 3, syncListener)
+            }
+            ERROR_DURING_VERIFICATION -> result.error?.let { error ->
+                Log.v("eBestSample", "error: ${error.errorType.name}")
+            }
         }
+    }
 
-        override fun onUnauthorized(deviceName: String?) {
-            Toast.makeText(applicationContext, "Unauthorized: $deviceName", Toast.LENGTH_SHORT).show()
+    private val syncListener = object: NexoSyncListener {
+
+        override fun onSuccess(result: String?) {
+            Log.v("eBestSample", "success: $result")
         }
 
         override fun onError(error: NexoError) {
-            Toast.makeText(applicationContext, error.errorType.name, Toast.LENGTH_SHORT).show()
+            Log.v("eBestSample", "error: ${error.errorType.name}")
         }
 
+        override fun onCoolerProgress(nexoId: String?, progress: Float) {
+            Log.v("eBestSample", "$nexoId -> progress: ${progress * 100}%")
+        }
     }
 
 }
+
+
